97e44f26cf271f38e81e175f7512621a79c1e55a
bazhekabro-ai <bazhekabro@gmail.com>
2025-09-16 21:21:58 +0500

    Delete main1.py

--- main1.py
+++
@@ -1,8 +0,0 @@
-a=float(input("Введите число a: "))
-b=float(input("Введите число b: "))
-c=float(input("Введите число c: "))
-p=(a+b+c)/2
-S=(p*(p-a)*(p-b)*(p-c))
-rounded_S = S ** 0.5
-rounded_S = round(S, 2)
-print(p, rounded_S)
